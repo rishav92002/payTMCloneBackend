1a885dc6d44e551f34c968a1b684fabd8a373b74
Rishav Kumar <119895945+rishav92002@users.noreply.github.com>
2026-01-20 03:28:51 +0530

    Added check for if Bearer is present in the token or not

--- middlewares/userMiddleWare.js
+++ middlewares/userMiddleWare.js
@@ -3,6 +3,10 @@ const app_config = require("../configs/config");
 
 const authenticateUser = async (req,res,next)=>{
     try{
+        const authorizationHeader = req.headers.authorization;
+        if(!authorizationHeader || !authorizationHeader.startsWith("Bearer")){
+            return res.status(401).json({message:"Unauthorized: Invalid token format or no token provided"});
+        }
         const token = req.headers.authorization?.split(" ")[1];
         if(!token){
             return  res.status(401).json({message:"Unauthorized: No token provided"});
