aad3f9ed8c056b57893139eb9fc2c6f0b8461318
Rishav Kumar <119895945+rishav92002@users.noreply.github.com>
2026-01-25 20:29:42 +0530

    added get user by id controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -130,9 +130,26 @@ const handleFindUserBytext = async (req,res)=>{
         res.status(500).json({message:"Internal Server Error",err});
     }
 }
+const handleGetUser = async (req,res)=>{
+    try{
+        const userId = req.user.id;
+        const user = await User.findById({_id:userId})
+        if(!user){
+            return res.status(404).json({
+                message: 'user not found'
+            })
+        }
+        return res.status(200).json({
+            user
+        })
+    }catch(err){
+        res.status(500).json({message:"Internal Server Error",err});
+    }
+}
 module.exports = {
     handleUserSignUp,
     handleUserLogIn,
     handleUserProfileEdit,
-    handleFindUserBytext
+    handleFindUserBytext,
+    handleGetUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {handleUserSignUp,handleUserLogIn,handleUserProfileEdit,handleFindUserBytext} = require("../controllers/userControllers");
+const {handleUserSignUp,handleUserLogIn,handleUserProfileEdit,handleFindUserBytext,handleGetUser} = require("../controllers/userControllers");
 const {authenticateUser} = require("../middlewares/userMiddleWare");
 
 
@@ -14,5 +14,6 @@ router.post("/signup",handleUserSignUp);
 router.post("/login",handleUserLogIn);
 router.put("/edit-profile",authenticateUser,handleUserProfileEdit);
 router.get("/bulkSearch",authenticateUser,handleFindUserBytext);
+router.get("/getuser",authenticateUser,handleGetUser);
 
 module.exports = router;
